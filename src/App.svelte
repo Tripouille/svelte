f5c3988396f40452e87331c4e647fea3ffd17fd1
Tripouille <jmichel.gambard@gmail.com>
2022-08-27 20:10:37 +0200

    slot

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,51 @@
 <style>
-main {
-  text-align: center;
-  padding: 1em;
-  max-width: 240px;
-  margin: 0 auto;
-}
-
-h1 {
-  color: #ff3e00;
-  text-transform: uppercase;
-  font-size: 4em;
-  font-weight: 100;
-}
-
-@media (min-width: 640px) {
   main {
-    max-width: none;
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
   }
-}
 </style>
 
 <script>
-import Modal from "./Modal.svelte";
+  import Modal from "./Modal.svelte";
 
-let people = [
-  { name: "yoshi", beltColour: "black", age: 25, id: 1 },
-  { name: "mario", beltColour: "orange", age: 45, id: 2 },
-  { name: "luigi", beltColour: "brown", age: 35, id: 3 },
-];
+  let people = [
+    { name: "yoshi", beltColour: "black", age: 25, id: 0 },
+    { name: "mario", beltColour: "orange", age: 45, id: 1 },
+    { name: "luigi", beltColour: "brown", age: 35, id: 2 },
+  ];
 
-function deletePersonById(id) {
-  return () => {
-    people = people.filter((person) => person.id !== id);
-  };
-}
-let num = 4;
-let showModal = true;
+  function deletePersonById(id) {
+    return () => {
+      people = people.filter((person) => person.id !== id);
+    };
+  }
+  let num = 4;
+  let showModal = false;
+  let nameInput;
+  function toggleModal() {
+    showModal = !showModal;
+  }
+
+  function addNewPerson({ target }) {
+    const { value: name } = target.name;
+    const { value: beltColour } = target.beltColour;
+    people = people.concat({ name, beltColour, age: 18, id: people.length });
+    showModal = false;
+  }
 </script>
 
-<label for="show-modal">
-	Show Modal
-  <input type="checkbox" id="show-modal" bind:checked={showModal} />
-</label>
-
-<Modal showModal={showModal} />
-
 <main>
+  <Modal {showModal} isPromo on:click={toggleModal}>
+    <h3>Add a New Person</h3>
+    <form on:submit|preventDefault={addNewPerson}>
+      <input type="text" placeholder="name" id="name" />
+      <input type="text" placeholder="belt colour" id="beltColour" />
+      <button type="submit">Add Person</button>
+    </form>
+  </Modal>
+  <button on:click={toggleModal}>Show New Person modal</button>
   {#each people as person (person.id)}
     <div>
       <h4>{person.name}</h4>
@@ -55,7 +55,7 @@ let showModal = true;
       <p class="badge" style="color: {person.beltColour}">
         {person.age} years old, {person.beltColour} belt.
       </p>
-      <button on:click="{deletePersonById(person.id)}">delete</button>
+      <button on:click={deletePersonById(person.id)}>delete</button>
     </div>
   {:else}
     <p>No people found.</p>
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,13 +1,37 @@
-<style></style>
+<style>
+  .backdrop {
+    position: fixed;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.6);
+  }
+
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    margin: 10%;
+    text-align: center;
+    background-color: white;
+    font-weight: bold;
+    font-size: x-large;
+  }
+
+  .promo {
+    background-color: crimson;
+    color: white;
+  }
+</style>
 
 <script>
-export let showModal = true;
+  export let showModal = false;
+  export let isPromo = false;
 </script>
 
 {#if showModal}
-  <div class="backdrop">
-    <div class="modal">
-      <p>Sign up for offers!</p>
+  <div class="backdrop" on:click|self>
+    <div class="modal" class:promo={isPromo}>
+      <slot />
     </div>
   </div>
 {/if}
